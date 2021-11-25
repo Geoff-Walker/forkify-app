6592cf9b6fa2cd54d8a9b7715d8d3c21751d5ee1
Geoff-Walker <geoffwalker1980@gmail.com>
2021-11-25 12:45:55 +0000

    Implementing Search Results Part 2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,25 +1,24 @@
+import icons from 'url:../../img/icons.svg'; //// Parcel 2
+
 class SearchView {
-    #parentEL = document.querySelector('.search');
+	_parentEL = document.querySelector('.search');
 
-    getQuery() {
-        const query = this.#parentEL.querySelector('.search__field').value;
-        this.#clearInput();
-        return query;
-    }
+	getQuery() {
+		const query = this._parentEL.querySelector('.search__field').value;
+		this._clearInput();
+		return query;
+	}
 
-    #clearInput() {
-        this.#parentEL.querySelector('.search__field').value = '';
-    }
+	_clearInput() {
+		this._parentEL.querySelector('.search__field').value = '';
+	}
 
-    addhandlerSearch(handler) {
-        this.#parentEL.addEventListener('submit', function(e) {
-            e.preventDefault();
-            handler()
-        })
-    }
-
-
-
+	addhandlerSearch(handler) {
+		this._parentEL.addEventListener('submit', function(e) {
+			e.preventDefault();
+			handler();
+		});
+	}
 }
 
 export default new SearchView();
