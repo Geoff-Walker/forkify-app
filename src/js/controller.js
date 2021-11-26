e61b18e57c8267a7d6699be32f353d16f17379f2
Geoff-Walker <geoffwalker1980@gmail.com>
2021-11-26 13:21:29 +0000

    Developing a Dom Updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,9 @@ const controlRecipes = async function() {
 
 		if (!id) return;
 		recipeView.renderSpinner();
+		/// results view to mark selected search results
+		resultsView.update(model.getSearchResultsPage());
+
 		/////// 1. Loading a Recipe
 
 		await model.loadRecipe(id);
@@ -57,8 +60,18 @@ const controlPagination = function(goToPage) {
 	paginationView.render(model.state.search);
 };
 
+const controlServings = function(newServings) {
+	//Update the recipe servings in the state
+	model.updateServings(newServings);
+	///Update the recipe view
+
+	// recipeView.render(model.state.recipe);
+	recipeView.update(model.state.recipe);
+};
+
 const init = function() {
 	recipeView.addHandlerRender(controlRecipes);
+	recipeView.addHandlerUpdateServings(controlServings);
 	searchView.addhandlerSearch(controlSearchResults);
 	paginationView.addHandlerClick(controlPagination);
 };
