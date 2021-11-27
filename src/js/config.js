35e6bcba3e4461a786fffa62a079b006885454e7
Geoff-Walker <geoffwalker1980@gmail.com>
2021-11-27 14:42:00 +0000

    Uploading a new Recipe part 2

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const API_KEY = 'b265d42a-6f75-41a1-afd6-b570c49e2cd2';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -90,9 +91,27 @@ const controlBookmarks = function() {
 	bookmarksView.render(model.state.bookMarks);
 };
 
-const controlAddRecipe = function(newRecipe) {
-	console.log(newRecipe);
+const controlAddRecipe = async function(newRecipe) {
+	try {
+		// render spinner
+		addRecipeView.renderSpinner();
+		await model.uploadRecipe(newRecipe);
+		console.log(model.state.recipe);
 
+		// Render Recipe
+
+		recipeView.render(model.state.recipe);
+
+		//Success Message
+		addRecipeView.renderMessage();
+		// close form window
+		setTimeout(function() {
+			addRecipeView.toggleWindow();
+		}, MODAL_CLOSE_SEC * 1000);
+	} catch (err) {
+		console.log('😁', err);
+		addRecipeView.renderError(err.message);
+	}
 	/// Upload the new recipe
 };
 
