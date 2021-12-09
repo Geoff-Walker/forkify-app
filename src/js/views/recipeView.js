14f33d3ab4ff09e0a6567dc2f0318e3e5584bcfb
Geoff-Walker <geoffwalker1980@gmail.com>
2021-12-09 12:21:45 +0000

    changed fractional to Fraction.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,8 @@
 import icons from 'url:../../img/icons.svg'; //// Parcel 2
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 import View from './View.js';
+
+const Fraction = require('fraction.js');
 
 class RecipeView extends View {
 	_parentElement = document.querySelector('.recipe');
@@ -114,7 +116,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+      <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
